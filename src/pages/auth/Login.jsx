93b5b6035583485838394c27b4432a3410c7ae0e
arnab-agavian <arnab.chaulya@agavian.in>
2025-09-10 00:24:33 +0530

    updates

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
   return (
-    <div className='login-page' style={{padding:'1rem'}}>
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center',}}>
-            <div style={{width:'100%', margin:'0 auto'}}>
-                <div className='logo-header'>
-                    <img src="/images/logo-2.png" alt="logo" width="100%" />
+    <div className='login-page' style={{padding:'1rem', backgroundImage:`url('/images/login-background.png')`, backgroundSize:'cover'}}>
+        <div className='container'>
+            <div style={{display:'flex', justifyContent:'center', alignItems:'center', height:'100%'}}>
+                <div className='video-container' >                    
+                    <div>
+                        <video src="/images/login-page-video.mp4" autoPlay loop></video>
+                    </div>
                 </div>
-                <h2 className='hd-c'>Login to StudyCraft</h2>
-            </div>
-            <div className='login-form-container' style={{width:'100%', margin:'0 auto'}}>
-                <div>
-                    <form action="">
-                        <div className='form-group'>
-                            <label htmlFor="">Email</label>
-                            <input type="text" placeholder='Email' />
+                <div className='login-form-container'>
+                    <div className='login-header'>
+                        <div className='logo-header'>
+                            <img src="/images/logo-2.png" alt="logo" width="100%" />
                         </div>
-                    </form>
+                        <h2 className='hd-c'>Login to StudyCraft</h2>
+                    </div>
+                    <div className='login-form'>
+                        <form action="">
+                            <div className='form-group'>
+                                <label htmlFor="">Email</label>
+                                <input type="text" placeholder='Email' />
+                            </div>
+                            <div className='form-group'>
+                                <label htmlFor="">Password</label>
+                                <input type="password" placeholder='password' />
+                            </div>
+                            <p className='hd-c' style={{textAlign:"center", marginBottom:'1rem'}}>New user in StudyCraft? <Link to='/register' style={{color:'#22218c'}}>resister</Link></p>
+                            <div className='form-group'>
+                                <button type='submit' className='btn-theme-three' style={{width:'100%'}}>Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
