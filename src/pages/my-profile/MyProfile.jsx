a783970392fa8ed4d39b308f2d8e1c640b72902c
arnab-agavian <arnab.chaulya@agavian.in>
2025-09-22 02:52:10 +0530

    updates

--- src/pages/my-profile/MyProfile.jsx
+++ src/pages/my-profile/MyProfile.jsx
@@ -1,7 +1,46 @@
 import React from 'react'
+import '../../styles/Profile.css';
+import { TopCard } from '../../components/my-profile/TopCard'
+import { Divider } from '@mui/material';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { MyProfileInfo } from '../../components/my-profile/MyProfileInfo';
 
 export const MyProfile = () => {
   return (
-    <div>MyProfile</div>
+    <section className='profile-section' style={{padding:'1rem'}}>
+      <TopCard />
+      <Divider />
+      <div className='profile-tab-section'>
+        <Tabs>
+          <div className='profile-info-tab-outer'>
+            <div className='profile-info-tab'>
+              <TabList>
+                <Tab><span className='profile-tab'>My account info</span></Tab>
+                <Tab><span className='profile-tab'>My Orders</span></Tab>
+                <Tab><span className='profile-tab'>My addresses</span></Tab>
+                <Tab><span className='profile-tab'>My payments</span></Tab>
+              </TabList>
+            </div>
+          </div>
+          <div className='product-additional-info-content' style={{margin:'1rem 0 2rem 0'}}>
+            <TabPanel>
+              <MyProfileInfo />
+            </TabPanel>
+            <TabPanel>
+              <div className=''>
+                <p className='prodcut-description hd-c'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam et praesentium velit beatae veritatis reiciendis non voluptatem quo, perferendis magni voluptatum dolores. Reiciendis quasi obcaecati facere ducimus assumenda, quas sed voluptatibus vel quis harum repellat earum tempora. Quod autem earum enim possimus laudantium non voluptatum, est illum exercitationem. Cupiditate, quam.</p>
+                <p className='prodcut-description hd-c'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam et praesentium velit beatae veritatis reiciendis non voluptatem quo, perferendis magni voluptatum dolores. Reiciendis quasi obcaecati facere ducimus assumenda, quas sed voluptatibus vel quis harum repellat earum tempora. Quod autem earum enim possimus laudantium non voluptatum, est illum exercitationem. Cupiditate, quam.</p>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className=''>
+                <p className='prodcut-description hd-c'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam et praesentium velit beatae veritatis reiciendis non voluptatem quo, perferendis magni voluptatum dolores. Reiciendis quasi obcaecati facere ducimus assumenda, quas sed voluptatibus vel quis harum repellat earum tempora. Quod autem earum enim possimus laudantium non voluptatum, est illum exercitationem. Cupiditate, quam.</p>                        
+
+              </div>
+            </TabPanel>    
+          </div>
+        </Tabs>
+      </div>
+    </section>
   )
 }
