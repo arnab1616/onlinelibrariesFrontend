56edb32bcff20c2d8bdc07f87a570d1d7f47c58d
arnab-agavian <arnab.chaulya@agavian.in>
2025-09-25 03:03:15 +0530

    updates

--- src/pages/my-profile/MyProfile.jsx
+++ src/pages/my-profile/MyProfile.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../styles/Profile.css';
 import { TopCard } from '../../components/my-profile/TopCard'
 import { Divider } from '@mui/material';
@@ -9,22 +9,37 @@ import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import FavoriteBorderRoundedIcon from '@mui/icons-material/FavoriteBorderRounded';
 import { MyAddresses } from '../../components/my-profile/MyAddresses';
 import { MyPayments } from '../../components/my-profile/MyPayments';
+import { MyFavorites } from '../../components/my-profile/MyFavorites';
+import { useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTabIndex } from '../../redux/headerSlice';
 
 export const MyProfile = () => {
+  const { tabIndex } = useSelector((state) => state.header);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.state?.tabIndex !== undefined) {
+      dispatch(setTabIndex(location.state.tabIndex));
+    }
+  }, [location.state]);
   return (
     <section className='profile-section' style={{padding:'1rem'}}>
       <TopCard />
       <Divider />
       <div className='profile-tab-section'>
-        <Tabs>
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => dispatch(setTabIndex(index))}>
           <div className='profile-info-tab-outer'>
             <div className='profile-info-tab'>
               <TabList>
-                <Tab><span className='profile-tab'><ManageAccountsIcon color='inherit' fontSize='inherit' /> My account info</span></Tab>
+                <Tab  ><span className='profile-tab'><ManageAccountsIcon color='inherit' fontSize='inherit' /> My account info</span></Tab>
                 <Tab><span className='profile-tab'><ShoppingBagIcon color='inherit' fontSize='inherit' /> My Orders</span></Tab>
                 <Tab><span className='profile-tab'><LocationOnIcon color='inherit' fontSize='inherit' /> My addresses</span></Tab>
+                <Tab><span className='profile-tab'><FavoriteBorderRoundedIcon color='inherit' fontSize='inherit' /> My Favorites</span></Tab>
                 <Tab><span className='profile-tab'><AccountBalanceWalletIcon color='inherit' fontSize='inherit' /> My payments</span></Tab>
               </TabList>
             </div>
@@ -38,6 +53,9 @@ export const MyProfile = () => {
             </TabPanel>
             <TabPanel>
               <MyAddresses />
+            </TabPanel>
+            <TabPanel>
+              <MyFavorites />
             </TabPanel>
             <TabPanel>
               <MyPayments />
